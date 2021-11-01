3748abb475e7b9842e6b44bd838e6c3e286ca4a2
Mike M <61656324+phantasmagoriadigital@users.noreply.github.com>
2021-11-01 18:54:28 +0530

    ✨ New Feature: Story Source in Story

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,19 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     '@pxtrn/storybook-addon-docs-stencil',
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
+    // '@storybook/addon-storysource',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          // test: [/\.stories\.jsx?$/], This is default
+          // include: [path.resolve(__dirname, '../src')], // You can specify directories
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false },
+        },
+      },
+    },
   ]
 }
